04cb489eeaf00d325f07afdf86cde04f83f299d9
Dikri Ghaniyyu Rahman <dikrighon@gmail.com>
2024-09-24 14:00:45 +0700

    big commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-// ini javascript
+function replaceName() {
+  let name = prompt("siapakah nama anda?", "");
+  document.getElementById("name").innerHTML = name;
+}
+
+document.getElementById("tombol").addEventListener("click", function () {
+  replaceName();
+});
