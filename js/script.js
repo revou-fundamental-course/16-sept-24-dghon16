83e6e00588692a4f22927f9739d0e010ad98c0ee
Dikri Ghaniyyu Rahman <dikrighon@gmail.com>
2024-09-26 11:36:54 +0700

    falidateform commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-replaceName();
+// validasi nama
+// replaceName();
 function replaceName() {
   let name = prompt("Masukan nama anda!");
   console.log(name);
   document.getElementById("user-name").innerHTML = name;
 }
 
-// function validateForm(){
-// let inputName = document.getElementById("input-name").value
-// }
+// validasi form kontak
+function validateMessage() {
+  const name = document.forms["message-form"]["full-name"].value;
+  const birthdate = document.forms["message-form"]["birth-date"].value;
+  const gender = document.forms["message-form"]["gender"].value;
+  const messages = document.forms["message-form"]["messages"].value;
+
+  if (name == "" || birthdate == "" || gender == "" || messages == "") {
+    alert("input kosong");
+    return false;
+  }
+  setSenderUI(name, birthdate, gender, messages);
+
+  return false;
+}
+
+function setSenderUI(name, birthdate, gender, messages) {
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-birth-date").innerHTML = birthdate;
+  document.getElementById("sender-gender").innerHTML = gender;
+  document.getElementById("sender-messages").innerHTML = messages;
+}
